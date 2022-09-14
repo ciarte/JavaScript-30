e0fbbb6a94719adfea1541bed1f01198d50128da
Gabriel Iciarte <gabriel_viki@hotmail.com>
2022-09-14 20:20:44 -0300

    drum kit

--- 01-JavaScript DrumKit/funcionalidad/functions.js
+++ 01-JavaScript DrumKit/funcionalidad/functions.js
@@ -1,28 +1,24 @@
-
-function tecla(e){
-  const sonido = document.querySelector(`audio[data-key='${e.keyCode}']`)
-  const key = document.querySelector(`div[data-key='${e.keyCode}']`)
-  if(!sonido) return;
+function tecla(e) {
+  const sonido = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+  if (!sonido) return;
   sonido.currentTime = 0;
   sonido.play();
-  key.classList.add('playing')
-  console.log(e)
+  key.classList.add("playing");
+  console.log(e);
 }
 
 function removeTransition(e) {
-  if(e.propertyName !== 'transform') return;
-  this.classList.remove('playing')
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
-document.addEventListener('keydown', tecla)
-
-
+document.addEventListener("keydown", tecla);
 
 module.exports = {
   tecla,
   removeTransition,
-  
-}
+};
